92586a96499527764385398e37d1a2ca6d8dbcaf
Alicia <alicia.rsouza@gmail.com>
2023-01-15 20:41:17 -0300

    feat: finalização

--- ToDo/src/components/Task/index.tsx
+++ ToDo/src/components/Task/index.tsx
@@ -1,6 +1,5 @@
 import { TaskStyled } from "./styles";
 import { Trash } from "phosphor-react";
-import { useState } from "react";
 
 interface TaskProps {
   task: string;
@@ -9,18 +8,12 @@ interface TaskProps {
 }
 
 export const Task = (props: TaskProps) => {
-  const [isActive, setIsActive] = useState(false);
-  const handleFinish = () => {
-    setIsActive(!isActive)
-  };
-
-
   return (
     <TaskStyled>
       <label className="container">
         <input type="checkbox" />
         <span className="text">{props.task}</span>
-        <span className="checkmark" onClick={handleFinish}></span>
+        <span className="checkmark"></span>
       </label>
       <button onClick={() => props.handleDelete(props.numero)}>
         <Trash className="trash" size={20} weight="regular" />
